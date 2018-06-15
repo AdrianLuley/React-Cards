9025b2742d2ecf7640e600d4987c8c491a62b73b
Adrian Luley <amluley@comcast.net>
2018-06-15 09:54:52 -0400

    first

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ import React from 'react';
 const CardGroup = (props) => (
     <div className="cardGroup">
         {props.children}
-        <Card price="Free!" cardDescription="Trail" icon="fa-thumbs-o-up"/>
-        <Card price="$10.00" cardDescription="Basic tier" hint="(most popular)" icon="fa-trophy"/>
-        <Card price="$6000.00" cardDescription="Advanced tier" hint="(only for enterprise-level professionals)" icon="fa-bolt"/>
+        <Card price="Free!" description="Trail" icon="fa-thumbs-o-up"/>
+        <Card price="$10.00" description="Basic tier" hint="(most popular)" icon="fa-trophy"/>
+        <Card price="$6000.00" description="Advanced tier" hint="(only for enterprise-level professionals)" icon="fa-bolt"/>
         
     </div>
 );
@@ -25,11 +25,11 @@ const CardGroup = (props) => (
 const Card = (props) => (
     
     <div className="card cardGroup__card">
-             <div className="card__description cardGroup__cardDescription">
+             <div className="card__description cardGroup__description">
                 <div className={"icon fa " + props.icon + " card__descriptionIcon" } />
                 
                  <div className="card__descriptionText">
-                     {props.cardDescription}
+                     {props.description}
                      <br/>
                      {props.hint}
                  </div>
@@ -40,39 +40,7 @@ const Card = (props) => (
 
 const App = () => (
     <CardGroup/>
-    // <div className="cardGroup">
-    //     <div className="card cardGroup__card">
-    //         <div className="card__description cardGroup__cardDescription">
-    //             <div className="icon fa fa-thumbs-o-up card__descriptionIcon" />
-    //             <div className="card__descriptionText">
-    //                 Trial
-    //             </div>
-    //         </div>
-    //         <div className="card__price">Free!</div>
-    //     </div>
-    //     <div className="card cardGroup__card">
-    //         <div className="card__description cardGroup__cardDescription">
-    //             <div className="icon fa fa-trophy card__descriptionIcon" />
-    //             <div className="card__descriptionText">
-    //                 Basic tier
-    //                 <br/>
-    //                 (most popular)
-    //             </div>
-    //         </div>
-    //         <div className="card__price">$10.00</div>
-    //     </div>
-    //     <div className="card cardGroup__card">
-    //         <div className="card__description cardGroup__cardDescription">
-    //             <div className="icon fa fa-bolt card__descriptionIcon" />
-    //             <div className="card__descriptionText">
-    //                 Advanced tier
-    //                 <br/>
-    //                 (only for enterprise-level professionals)
-    //             </div>
-    //         </div>
-    //         <div className="card__price">$6,000.00</div>
-    //     </div>
-    // </div>
+   
 );
 
 export default App;
